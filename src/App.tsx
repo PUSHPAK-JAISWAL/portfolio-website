d16984e9e286baee58c7f141addf384719034ad6
Pushpak Jaiswal <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2026-07-23 15:06:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
         <Toaster />
         <Sonner />
         {booting && <BootLoader onDone={() => setBooting(false)} />}
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/admin" element={<Admin />} />
